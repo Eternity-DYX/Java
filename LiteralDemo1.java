6ba432ed8d1e84d31f8a896c7549585744010a1a
DylanDaiCoder <whwcaabtyk@163.com>
2026-02-06 23:22:18 +0800

    Add files via upload

---
+++ LiteralDemo1.java
@@ -0,0 +1,19 @@
+package com.itheima.literal;
+
+public class LiteralDemo1 {
+    static void main(String[] args) {
+        //输出恐龙的信息：霸王龙 8岁 11.5米 公
+
+        //输出恐龙的名字
+        System.out.println ("霸王龙");
+
+        //输出恐龙的年龄
+        System.out.println (8+"岁");
+
+        //输出恐龙的长度
+        System.out.println (11.5+"米");
+
+        //输出恐龙的性别
+        System.out.println ("公");
+    }
+}
