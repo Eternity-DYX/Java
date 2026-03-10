24039b0a151a30ac763ea4d2beb1edea81b3a0d4
DylanDaiCoder <whwcaabtyk@163.com>
2026-03-10 23:01:52 +0800

    Add files via upload

---
+++ BasicCode/Loopfor/ForDemo2.java
@@ -0,0 +1,20 @@
+package com.DYX.loopfor;
+
+public class ForDemo2 {
+    public static void main(String[] args) {
+        /*
+            需求：在实际开发中，如果要获取一个范围中的每一个数据时，就会用到循环
+            需求1：打印1-5
+            需求2：打印5-1
+        */
+
+        //打印1-5
+        // 循环的开始条件
+        // 循环的结束条件
+        // 重复执行的代码
+        for(int i = 1; i <= 5; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
