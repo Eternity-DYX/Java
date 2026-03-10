ad0a1faead40fcd5b1db7d1c4f719a23b590060c
DylanDaiCoder <whwcaabtyk@163.com>
2026-03-10 23:00:20 +0800

    Create ForDemo1.java

---
+++ BasicCode/Loopfor/ForDemo1.java
@@ -0,0 +1,25 @@
+package com.DYX.loopfor;
+
+public class ForDemo1 {
+    public static void main(String[] args) {
+        /*
+            for循环
+                for(初始化;条件;更新)
+                {
+                    循环体
+                }
+        */
+
+        //1.定义变量记录起始值
+        int start = 1;
+        //2.定义变量记录结束值
+        int end = 10;
+        //3.定义变量记录和
+        int sum = 0;
+        //4.循环求和
+        for(int i = start; i <= end; i++)
+        {
+            System.out.println("游戏人物跳跃");
+        }
+    }
+}
