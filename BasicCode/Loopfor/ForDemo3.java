1a4ed0e714e89e28afc8ebc0e47da748fb16d7c9
DylanDaiCoder <whwcaabtyk@163.com>
2026-03-10 23:03:16 +0800

    Add files via upload

---
+++ BasicCode/Loopfor/ForDemo3.java
@@ -0,0 +1,12 @@
+package com.DYX.loopfor;
+
+public class ForDemo3 {
+    public static void main(String[] args) {
+        //需求：打印5-1
+
+        for(int i = 5; i >= 1; i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
