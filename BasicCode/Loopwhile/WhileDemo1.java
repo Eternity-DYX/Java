b31d019bc8a5f9dee1155b074a5b6b06943c8a82
DylanDaiCoder <whwcaabtyk@163.com>
2026-03-17 11:40:54 +0800

    Create WhileDemo1.java

---
+++ BasicCode/Loopwhile/WhileDemo1.java
@@ -0,0 +1,15 @@
+package com.DYX.loopwhile;
+
+public class WhileDemo1 {
+    static void main(String[] args) {
+        /*
+                利用while循环，实现游戏中跳跃10次，用输出语句模拟跳跃的逻辑
+        */
+    int i = 1;
+    while(i <= 10)
+    {
+        System.out.println("游戏人物跳跃了一次");
+        i++;
+    }
+    }
+}
