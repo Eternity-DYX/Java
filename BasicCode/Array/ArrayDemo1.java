b5261302e612d52dddf5b340a02580d4f1fee4ed
DylanDaiCoder <whwcaabtyk@163.com>
2026-03-31 10:56:59 +0800

    Create ArrayDemo1.java

---
+++ BasicCode/Array/ArrayDemo1.java
@@ -0,0 +1,21 @@
+package com.DYX.array;
+
+public class ArrayDemo1 {
+    static void main(String[] args) {
+        /*
+                数组的静态初始化：
+                    1.定义数组存储3位同学的年龄
+                    2.定义数组存储3位同学的身高
+                    3.定义数组存储3位同学的名字
+        */
+    int []ageArr1={18,19,20};
+    int []ageArr2=new int[]{18,19,20};
+
+    String []nameArr1={"小王","小李","小张"};
+    String []nameArr2=new String[]{"小王","小李","小张"};
+
+    double []heightArr1={1.68,1.69,1.70};
+    double []heightArr2=new double[]{1.68,1.69,1.70};
+
+    }
+}
